81962f6179c49476b5a9430c31f27b1421f4c2c3
Matt Sage <mathew.sage@vanquisbank.co.uk>
2020-09-03 10:05:40 +0100

    added web server

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"go/demo/webservice/models"
+	"go/demo/webservice/controllers"
+	"net/http"
 )
 
 func main() {
-	u := models.User{
-		ID:        2,
-		FirstName: "Matt",
-		LastName:  "Sage",
-	}
-	fmt.Println(u)
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
 }
